a58095a1e77f77abc2107d32060ae29ab0ac1dde
Exenreco Bell <exenreco19@yahoo.com>
2024-08-12 19:11:34 -0500

    Week 1 - hands on 1 - 2 finished!

---
+++ week-1/chapter-1/hands-on-project-1-2/project01-02_txt.js
@@ -0,0 +1,19 @@
+/*  JavaScript 7th Edition
+    Chapter 1
+    Hands-On Project 1-2
+
+    Author: Exenreco Bell
+    Date:   08/12/2024
+
+    Filename: project01-02.js
+*/
+
+//define variables for service name and service speed
+var service1Name = "Basic";
+var service2Name = "Express";
+var service3Name = "Extreme";
+var service4Name = "Ultimate";
+var service1Speed = "50 Mbsp";
+var service2Speed = "100 Mbsp";
+var service3Speed = "500 Mbsp";
+var service4Speed = "1 Gig";
