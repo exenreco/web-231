f9a2a68ccc4468f415b4b380c29309388e45154e
Exenreco Bell <exenreco19@yahoo.com>
2024-08-26 09:15:19 -0500

    Week 1 - hands on 1 - 2 updated!

--- week-1/project1/project01-02.js
+++ week-1/project1/project01-02.js
@@ -8,12 +8,12 @@
     Filename: project01-02.js
 */
 
-//define variables for service name and service speed
-var service1Name = "Basic";
-var service2Name = "Express";
-var service3Name = "Extreme";
-var service4Name = "Ultimate";
-var service1Speed = "50 Mbsp";
-var service2Speed = "100 Mbsp";
-var service3Speed = "500 Mbsp";
-var service4Speed = "1 Gig";
+let  //define variables for service name and service speed
+  service1Name = "Basic",
+  service2Name = "Express",
+  service3Name = "Extreme",
+  service4Name = "Ultimate",
+  service1Speed = "50 Mbsp",
+  service2Speed = "100 Mbsp",
+  service3Speed = "500 Mbsp",
+  service4Speed = "1 Gig";
